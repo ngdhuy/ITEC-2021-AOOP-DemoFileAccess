896f86942daaf3af8cbcb2123d0bff18071f9a77
Huy NGUYEN <ndhuy@fit.hcmus.edu.vn>
2021-08-20 16:05:25 +0700

    feature: navigate previous and next quest and using MDI Form

---
+++ DemoChangeQuestion/frmMain.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DemoChangeQuestion
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Enabled = false;
+            this.txtQuestion.Location = new System.Drawing.Point(25, 25);
+            this.txtQuestion.Multiline = true;
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(924, 176);
+            this.txtQuestion.TabIndex = 0;
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(25, 225);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(180, 61);
+            this.btnPrevious.TabIndex = 1;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(257, 225);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(180, 61);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(769, 225);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(180, 61);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 298);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.txtQuestion);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuestion;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ DemoChangeQuestion/frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoChangeQuestion
+{
+    public partial class frmMain : Form
+    {
+        private BindingList<String> lstQuestion = new BindingList<String>();
+        private int currentQuestionIndex;
+
+        public int CurrrentQuestionIndex
+        {
+            get { return currentQuestionIndex; }
+            set { 
+                currentQuestionIndex = value; 
+                txtQuestion.Text = lstQuestion[currentQuestionIndex].ToString();
+            }
+        }
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                String sQuestion = "Quest at " + i.ToString();
+                lstQuestion.Add(sQuestion);
+            }
+            this.CurrrentQuestionIndex = 0;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (CurrrentQuestionIndex <= 0)
+                MessageBox.Show("Cannot navigate to below 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                this.CurrrentQuestionIndex--;
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (CurrrentQuestionIndex >= lstQuestion.Count - 1)
+                MessageBox.Show("Cannot navigate out of range", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+                this.CurrrentQuestionIndex++;
+        }
+    }
+}
